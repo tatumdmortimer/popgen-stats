200de300d6dba762b11e980ac5894560b1d8f7bf
Tatum Mortimer <tatumdmortimer@gmail.com>
2014-04-28 17:04:38 -0500

    Added Fay and Wu's H

--- slidingWindowStats.py
+++ slidingWindowStats.py
@@ -7,6 +7,7 @@ import egglib
 
 # This script reads in an alignment and calculates diversity and selection
 # statistics based on the window width and window step given by the user.
+# Will calculate Fay and Wu's H if an outgroup is provided.
 
 def get_arguments(argv):
     if len(argv) == 0:
@@ -15,8 +16,9 @@ def get_arguments(argv):
     alignment = None
     winWidth = 1000
     winStep = 300
+    outgroup = None
     try:
-        opts, args = getopt.getopt(argv, "a:w:s:")
+        opts, args = getopt.getopt(argv, "a:w:s:o:")
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -27,13 +29,16 @@ def get_arguments(argv):
             winWidth = int(arg) 
         elif opt == '-s':
             winStep = int(arg)
-    return (alignment, winWidth, winStep)
+        elif opt == '-o':
+            outgroup = arg
+    return (alignment, winWidth, winStep, outgroup)
 
 def usage():
     print "slidingWindowStats.py\n \
         -a <fasta alignment>\n \
         -w <window width default = 1000>\n \
-        -s <window step default = 300>"
+        -s <window step default = 300>\n \
+        -o <outgroup>"
 
 def calc_stats(a):
     statDict = {}
@@ -41,25 +46,28 @@ def calc_stats(a):
     statDict['theta'] = polyDict['thetaW']
     statDict['pi'] = polyDict['Pi']
     statDict['tajimaD'] = polyDict['D']
+    statDict['FayWuH'] = polyDict['H']
     return statDict
 
-alignment, winWidth, winStep = get_arguments(sys.argv[1:])
+alignment, winWidth, winStep, outgroup = get_arguments(sys.argv[1:])
 
 if alignment is None:
     usage()
     sys.exit()
 
 outfile = open('windowStats_' + os.path.splitext(alignment)[0] + '.txt', 'w')
-outfile.write("Start\tStop\tTheta\tPi\tTajimasD\n")
+outfile.write("Start\tStop\tTheta\tPi\tTajimasD\tFay&WuH\n")
 align = egglib.Align(alignment)
 for i in range(align.ns()):
     align.sequence(i, sequence=align.sequence(i).upper())
+if outgroup is not None:
+    align.group(align.find(outgroup, strict = False), group = 999)
 start = 0
 stop = winWidth
 for window in align.slider(winWidth, winStep):
     stats = calc_stats(window)
     start += winStep
     stop += winStep
-    outfile.write("%i\t%i\t%s\t%s\t%s\n" % (start, stop, stats['theta'],
-    stats['pi'], stats['tajimaD']))
+    outfile.write("%i\t%i\t%s\t%s\t%s\t%s\n" % (start, stop, stats['theta'],
+    stats['pi'], stats['tajimaD'], stats['FayWuH']))
 outfile.close()
