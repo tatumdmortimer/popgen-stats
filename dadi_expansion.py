6e8744ffd0b9f77b9e41a00947073579b98f09d5
Tatum Mortimer <tatumdmortimer@gmail.com>
2016-08-18 10:40:17 -0500

    Now outputs SFS for combination of parameters that results in maximum log likelihood

--- dadi_expansion.py
+++ dadi_expansion.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 
-import sys
 import argparse
-from Bio.Phylo.PAML.chi2 import cdf_chi2
 import numpy
 from numpy import array
 import dadi
-from math import *
-import cmath
 from datetime import datetime
 
 
@@ -23,7 +19,9 @@ def get_args():
 
 def rep_ll_expansion(data, ns, expansion_params, expansion_upper_bound,
                      expansion_lower_bound, expansion_func_ex, pts_l):
-    expansion_vals = []
+    expansion_ll_vals = []
+    expansion_nu_vals = []
+    expansion_tau_vals = []
     outfile = open("dadi_expansionModel.txt", "w")
     outfile.write("nu\ttau\tll\n")
     for i in range(100):
@@ -41,10 +39,24 @@ def rep_ll_expansion(data, ns, expansion_params, expansion_upper_bound,
                 expansion_popt[0],
                 expansion_popt[1],
                 expansion_ll_opt))
-        expansion_vals.append(expansion_ll_opt)
-    avg_ll_expansion = sum(expansion_vals) / float(len(expansion_vals))
+        expansion_ll_vals.append(expansion_ll_opt)
+        expansion_nu_vals.append(expansion_popt[0])
+        expansion_tau_vals.append(expansion_popt[1])
     outfile.close()
-    return avg_ll_expansion
+    avg_ll_expansion = numpy.median(expansion_ll_vals)
+    avg_nu_expansion = numpy.median(expansion_nu_vals)
+    avg_tau_expansion = numpy.median(expansion_tau_vals)
+    max_ll_index, max_ll_expansion = max(
+        enumerate(expansion_ll_vals), key=lambda p: p[1])
+    max_nu_expansion = expansion_nu_vals[max_ll_index]
+    max_tau_expansion = expansion_tau_vals[max_ll_index]
+    return (
+        avg_ll_expansion,
+        avg_nu_expansion,
+        avg_tau_expansion,
+        max_ll_expansion,
+        max_nu_expansion,
+     max_tau_expansion)
 
 
 startTime = datetime.now()
@@ -63,9 +75,21 @@ ns = data.sample_sizes
 if args.fold:
     data = data.fold()
 
-avg_ll_expansion = rep_ll_expansion(data, ns, expansion_params, expansion_upper_bound,
-                                    expansion_lower_bound, expansion_func_ex, pts_l)
+avg_ll_expansion, avg_nu_expansion, avg_tau_expansion, max_ll_expansion, max_nu_expansion, max_tau_expansion = rep_ll_expansion(
+     data, ns, expansion_params, expansion_upper_bound, expansion_lower_bound, expansion_func_ex, pts_l)
 print "Average optimized log-likelihood:", avg_ll_expansion
 avg_AIC_expansion = 2 * k - 2 * avg_ll_expansion
 print "Average AIC:", avg_AIC_expansion
+
+print "Maximum optimized log-likelihood:", max_ll_expansion
+max_AIC_expansion = 2 * k - 2 * max_ll_expansion
+print "Optimal AIC:", max_AIC_expansion
+
+expansion_best_model = expansion_func_ex(
+    [max_nu_expansion, max_tau_expansion], ns, pts_l)
+expansion_sfs = dadi.Inference.optimally_scaled_sfs(
+    expansion_best_model, data)
+with open("expansionModelSFS.txt", "w") as sfsfile:
+    for i in range(1, len(expansion_sfs)-1):
+        sfsfile.write(str(expansion_sfs[i]) + "\n")
 print datetime.now() - startTime
